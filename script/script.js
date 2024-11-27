c6465c8fc1e69dd7fcc0b1b1d160bda8d34f9812
Ozodbek <jumakulovozodbek89email@example.com>
2024-11-27 23:47:02 +0500

    masala

--- script/script.js
+++ script/script.js
@@ -1,4 +1,11 @@
-
+function changeOrder(s, indexes) {
+    return indexes.map(i => s[i]).join("");
+  }
+  
+  let s = "acb";
+  let newS = changeOrder(s, [1, 2, 0]);
+  console.log(newS); 
+  
 
 
 
