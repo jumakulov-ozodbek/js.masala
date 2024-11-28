2733cf06f6f38bcef2b03aaf82377cc43c989cff
Ozodbek <jumakulovozodbek89email@example.com>
2024-11-28 11:41:25 +0500

    masla

--- script/script.js
+++ script/script.js
@@ -5,13 +5,44 @@
 //   let s = "acb";
 //   let son = changeOrder(s, [1, 2, 0]);
 //   console.log(son); 
-  function changeOrder(s, index) {
-    return index.map(i => s[i]).join("");
-  }
+  // function changeOrder(s, index) {
+  //   return index.map(i => s[i]).join("");
+  // }
   
-  let s = "acb";
-  let son = changeOrder(s, [2, 2, 0]);
-  console.log(son);
+  // let s = "acb";
+  // let son = changeOrder(s, [2, 2, 0]);
+  // // console.log(son);
+  // function changeOrder(s, index) {
+  //   return index.map(i => s[i]).join("");
+  // }
+  
+  // let s = "acb";
+  // let son = changeOrder(s, [0,1,2]);
+  // console.log(son);
+  function splitCharactersAndNumbers(input) {
+    const letters = [];
+    const numbers = [];
+
+    for (const char of input) {
+        if (!isNaN(char) && char !== ' ') {
+            numbers.push(char);
+        } else if (/[a-zA-Z]/.test(char)) {
+            letters.push(char);
+        }
+    }
+
+    return {
+        letters: letters.join(''),
+        numbers: numbers.join('')
+    };
+}
+
+
+const input = "a1b2c3d8d4";
+const result = splitCharactersAndNumbers(input);
+
+console.log("Raqamlar:", result.numbers);
+
 
 
 
