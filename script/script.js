43971ff40a804f2de8b2673098696b9a13ce514b
Ozodbek <jumakulovozodbek89email@example.com>
2024-11-28 11:26:01 +0500

    4get

--- script/script.js
+++ script/script.js
@@ -1,11 +1,17 @@
-function changeOrder(s, index) {
+// function changeOrder(s, index) {
+//     return index.map(i => s[i]).join("");
+//   }
+  
+//   let s = "acb";
+//   let son = changeOrder(s, [1, 2, 0]);
+//   console.log(son); 
+  function changeOrder(s, index) {
     return index.map(i => s[i]).join("");
   }
   
   let s = "acb";
-  let son = changeOrder(s, [1, 2, 0]);
-  console.log(son); 
-  
+  let son = changeOrder(s, [2, 2, 0]);
+  console.log(son);
 
 
 
