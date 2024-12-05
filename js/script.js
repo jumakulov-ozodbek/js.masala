3ec051eba946af1322d2c1f2471442b3d62baa93
Ozodbek <jumakulovozodbek89email@example.com>
2024-12-05 12:55:52 +0500

    sd

--- js/script.js
+++ js/script.js
@@ -188,32 +188,33 @@
 // console.log(team.sort((a,b)=>b.score-a.score));
 
 
-const market=[
-  {
-    name:"Mandarin",
-    price:50,
-    quantity:5,
-  },
-  {
-    name:"Apelsin",
-    price:40,
-    quantity:5,
-  },
-  {
-    name:"Limon",
-    price:80,
-    quantity:20,
-  },
-];
-let total=0;
-market.forEach((item)=>{
-  total+=item.price*item.quantity;
-})
-console.log("savatning narxi:",total,"som");
-const budget=20000;
-if(total<=budget){
-  console.log("Savatga qo'shildi.");
-}else{
-  console.log("Savatda qo'shilmadi.");
-}
-console.log("xaridor olgan muhsulotlar:",market);
+// const market=[
+//   {
+//     name:"Mandarin",
+//     price:50,
+//     quantity:5,
+//   },
+//   {
+//     name:"Apelsin",
+//     price:40,
+//     quantity:5,
+//   },
+//   {
+//     name:"Limon",
+//     price:80,
+//     quantity:20,
+//   },
+// ];
+// let total=0;
+// market.forEach((item)=>{
+//   total+=item.price*item.quantity;
+// })
+// console.log("savatning narxi:",total,"som");
+// const budget=20000;
+
+// if(total<=budget){
+//   console.log("Savatga qo'shildi.");
+// }else{
+//   console.log("Savatda qo'shilmadi.");
+// }
+// console.log("xaridor olgan muhsulotlar:",market);
